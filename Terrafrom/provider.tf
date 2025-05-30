04c8af25320661724796962bc3fd0001c043ab20
Hariom Rajpoot <Hariom.nitb@gmail.com>
2025-05-30 14:06:55 +0530

    added files and folder

--- Terrafrom/provider.tf
+++ Terrafrom/provider.tf
@@ -1,18 +1,3 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "availability_zones" {
-  default = ["us-east-1a"]
-}
-
-variable "public_subnets" {
-  default = ["10.0.1.0/24"]
-}
-variable "private_subnets" {
-  default = ["10.0.2.0/24"]
+provider "aws" {
+  region = "var.aws_region"
 }
